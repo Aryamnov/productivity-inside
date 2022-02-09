cec80cd2dc8db1b24ddfc3d7f8958f3f15376228
Andrey Aryamnov <aryamnov@gmail.com>
2022-02-09 23:24:44 +0700

    add: infinite scroll

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,33 +2,65 @@ import * as React from "react";
 import api from "../utils/api";
 import { Episode } from "./Episode";
 
-export interface episodes {
-  info: {
-    count: number;
-    next: string;
-    pages: number;
-    prev: any;
-  };
-  results: [];
-  next: string;
-}
-
 export interface IMainProps {}
 
 export function Main(props: IMainProps) {
-  const [episodes, setEpisodes] = React.useState<episodes>();
+  const [episodes, setEpisodes] = React.useState<Array<{}>>();
+  const [link, setLink] = React.useState(
+    "https://rickandmortyapi.com/api/episode?page=1"
+  );
+  const [isFetching, setIsFetching] = React.useState(true);
+  const [isAllCard, setIsAllCard] = React.useState(false);
 
   React.useEffect(() => {
-    api
-      .getEpisodes()
-      .then((res: episodes) => setEpisodes(res))
-      .catch((err) => console.log(err));
-  }, []);
+    if (isFetching && !isAllCard) {
+      //при запросе и наличию карточек на сервере
+      api
+        .getEpisodeUrl(link) //запрос на сервер
+        .then((res: any) => {
+          if (episodes) {
+            const newArr = episodes.concat(res.results);
+            setEpisodes(newArr);
+          } else {
+            setEpisodes(res.results);
+          }
+          if (res.info.next) {
+            setLink(res.info.next);
+          } else {
+            setIsAllCard(true); //если карточки на сервере закончились
+          }
+        })
+        .finally(() => {
+          setIsFetching(false);
+        });
+    }
+  }, [isFetching]);
+
+  const checkPosition = () => {
+    // проверяем положение на странице
+    const height = document.body.offsetHeight;
+    const screenHeight = window.innerHeight;
+    const scrolled = window.scrollY;
+    const threshold = height - screenHeight / 2;
+    const position = scrolled + screenHeight;
+    if (position >= threshold) {
+      //если перешли границу, то новый запрос на сервер
+      setIsFetching(true);
+    }
+  };
+
+  React.useEffect(() => {
+    //в зависимости от задачи, возможно, стоит добавить событие "resize"
+    window.addEventListener("scroll", checkPosition);
+    return function () {
+      document.removeEventListener("scroll", checkPosition);
+    };
+  }, []); //эффект добавляет слушателя по скролу
 
   return (
     <div className="main">
       {episodes
-        ? episodes.results.map((episode: any) => (
+        ? episodes.map((episode: any) => (
             <Episode episode={episode} key={episode.id} />
           ))
         : ""}
