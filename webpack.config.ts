b60594cd671ea8b5f472f65277f7e63d9980522a
Andrey Aryamnov <aryamnov@gmail.com>
2022-07-28 00:01:29 +0700

    fix: build index

--- webpack.config.ts
+++ webpack.config.ts
@@ -29,7 +29,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
-    publicPath: '/'
+    publicPath: ''
   },
   devServer: {
     static: path.join(__dirname, "build"),
